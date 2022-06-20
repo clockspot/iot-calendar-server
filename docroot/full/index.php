590153caf0f4ea640a4298a72142a9875abcd0fe
Luke McKenzie <luke@theclockspot.com>
2022-06-20 00:54:24 -0500

    Add sun; other small improvements

--- docroot/full/index.php
+++ docroot/full/index.php
@@ -7,57 +7,67 @@ $auth = $_REQUEST['auth'];
 require_once '../../settings.php';
 require_once '../../vendor/autoload.php';
 
-$prefs = json_decode(PREFS);
-$nws = json_decode(NWS);
-$cals = json_decode(CALS);
-
-foreach([$prefs,$nws,$cals] as $ar) if(!property_exists($ar,$auth)) die();
-
-$prefs = $prefs->$auth;
-$nws = $nws->$auth;
-$cals = $cals->$auth;
+$authkeys = json_decode(AUTHKEYS);
+if(!property_exists($authkeys,$auth)) die();
+$prefs = $authkeys->$auth;
 
 if(property_exists($prefs,'tz')) date_default_timezone_set($prefs->tz);
 if(!property_exists($prefs,'timeformat')) $prefs->timeformat = DEFAULT_TIME_FORMAT;
+if(!property_exists($prefs,'dateshortformat')) $prefs->dateshortformat = DEFAULT_DATE_SHORT_FORMAT;
 if(!property_exists($prefs,'days')) $prefs->days = DEFAULT_DAYS;
 
 //prepare the data structure that will be returned as JSON for display
 $c = array(); 
 $d = new DateTime();
 for($i=0; $i<$prefs->days; $i++){
-  if($i>0) $d->add(new DateInterval('P1D'));
   $date = new stdClass();
-  $date->weekday = $d->format("D");
+  if($i>0) $d->add(new DateInterval('P1D'));
+  $date->weekday = $d->format("l");
+  $date->weekdayShort = $d->format("D");
+  $date->weekdayRelative = ($i==0? 'Today': ($i==1? 'Tomorrow': $date->weekday));
   $date->date = $d->format("j");
-  $date->month = $d->format("M");
+  $date->dateShort = $d->format($prefs->dateshortformat);
+  $date->month = $d->format("F");
+  $date->monthShort = $d->format("M");
+  if($i==0) {
+    if(property_exists($prefs,'latitude') && property_exists($prefs,'longitude')) {
+      $date->sun = date_sun_info($d->format('U'),$prefs->latitude,$prefs->longitude);
+      foreach($date->sun as $k=>$s) {
+        if($s) $date->sun[$k] = date($prefs->timeformat, $s);
+      }
+    }
+  } 
   $date->weather = array();
   $date->events = array();
   $c[$d->format('Y-m-d')] = $date;
 }
 
 //Weather
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.weather.gov/gridpoints/".$nws."/forecast");
-$headers = array('User-Agent: '.NWS_USER_AGENT);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);  
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$out = curl_exec($ch);
-curl_close($ch);
-$r = json_decode($out);
-if(!(property_exists($r,'status') && property_exists($r,'detail')) && property_exists($r,'properties') && is_object($r->properties) && property_exists($r->properties,'periods') && is_array($r->properties->periods)){
-  foreach($r->properties->periods as $p) {
-    $dt = substr($p->startTime,0,10);
-    if(array_key_exists($dt,$c)) {
-      $w = new stdClass();
-      $w->name = $p->name;
-      $w->isDaytime = $p->isDaytime;
-      $w->temperature = $p->temperature;
-      $w->shortForecast = weatherReplace($p->shortForecast);
-      $c[$dt]->weather[] = $w;
-    }    
+if(property_exists($prefs,'nws')) {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "https://api.weather.gov/gridpoints/".$prefs->nws."/forecast");
+  $headers = array('User-Agent: '.NWS_USER_AGENT);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);  
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $out = curl_exec($ch);
+  curl_close($ch);
+  $r = json_decode($out);
+  if(!(property_exists($r,'status') && property_exists($r,'detail')) && property_exists($r,'properties') && is_object($r->properties) && property_exists($r->properties,'periods') && is_array($r->properties->periods)){
+    foreach($r->properties->periods as $p) {
+      $dt = substr($p->startTime,0,10);
+      if($p->name=='Overnight') continue; //don't bother with these
+      if(array_key_exists($dt,$c)) {
+        $w = new stdClass();
+        $w->name = $p->name;
+        $w->isDaytime = $p->isDaytime;
+        $w->temperature = $p->temperature;
+        $w->shortForecast = weatherReplace($p->shortForecast);
+        $c[$dt]->weather[] = $w;
+      }    
+    }
   }
-}
+} //end if nws specified
 
 function weatherReplace($in) {
   return str_replace('Showers And Thunderstorms','Rain',$in);
@@ -65,120 +75,129 @@ function weatherReplace($in) {
 
 //Calendars
 use ICal\ICal;
-
-$events = new stdClass();
-$events->events = array();
-
-foreach($cals as $cal) {
-  try {
-    $ical = new ICal('ICal.ics', array(
-      'defaultSpan'                 => 1,
-      'filterDaysAfter'             => $prefs->days,
-      'filterDaysBefore'            => $prefs->days,
-    ));
-    $ical->initUrl($cal->src);
-  } catch (Exception $e) {
-    die($e);
-  }
+if(property_exists($prefs,'cals') && is_array($prefs->cals) && sizeof($prefs->cals)) {
   
-  $ies = $ical->eventsFromInterval($prefs->days.' days');
-  //echo "<pre>"; var_dump($ies);
-  //echo "<pre>".json_encode($ies,JSON_PRETTY_PRINT)."</pre>";
-  foreach($ies as $ie) {
-    $event = new stdClass();
-    $event->summary = $ie->summary;
-    //can't use dtstart_tz because that appears to be in the calendar's zone, which may not be the desired zone
-    //so we'll use PHP DateTime objects to convert from the event zone to the desired zone (spec'd in settings)
-    //extract event's zone (no zone for allday events)
-    $dz = array_key_exists('TZID',$ie->dtstart_array[0])? $ie->dtstart_array[0]['TZID']: null;
-    //extract event's zoneless start and end times
-    $dstart = $dz? substr($ie->dtstart_array[3], strpos($ie->dtstart_array[3],':')+1): $ie->dtstart_array[3];
-    $dend = $dz? substr($ie->dtend_array[3], strpos($ie->dtend_array[3],':')+1): $ie->dtend_array[3];
-    //convert to datetime objects using event's zone
-    $dstart = $dz? new DateTime($dstart, new DateTimeZone($dz)): new DateTime($dstart);
-    $dend = $dz? new DateTime($dend, new DateTimeZone($dz)): new DateTime($dend);
-    //set datetime objects to the web app's default zone
-    $dstart->setTimezone(new DateTimeZone(date_default_timezone_get()));
-    $dend->setTimezone(new DateTimeZone(date_default_timezone_get())); //TODO more efficient way to do this?
-    //finally re-render strings
-    $event->dstart = $dstart->format('Y-m-d');
-    $event->ldstart = $dstart->format('Ymd');
-    $event->dend = $dend->format('Y-m-d');
-    $event->ldend = $dend->format('Ymd');
-    $event->allday = ($dz===null); //ASSUMES no zone means all day
-    if(INCLUDE_DURATION) {
+  $events = new stdClass();
+  $events->events = array();
+
+  foreach($prefs->cals as $cal) {
+    try {
+      $ical = new ICal('ICal.ics', array(
+        'defaultSpan'                 => 1,
+        'filterDaysAfter'             => $prefs->days,
+        'filterDaysBefore'            => $prefs->days,
+      ));
+      $ical->initUrl($cal->src);
+    } catch (Exception $e) {
+      die($e);
+    }
+    
+    $ies = $ical->eventsFromInterval($prefs->days.' days');
+    //echo "<pre>"; var_dump($ies);
+    //echo "<pre>".json_encode($ies,JSON_PRETTY_PRINT)."</pre>";
+    foreach($ies as $ie) {
+      $event = new stdClass();
+      $event->summary = $ie->summary;
+      //can't use dtstart_tz because that appears to be in the calendar's zone, which may not be the desired zone
+      //so we'll use PHP DateTime objects to convert from the event zone to the desired zone (spec'd in settings)
+      //extract event's zone (no zone for allday events)
+      $dz = array_key_exists('TZID',$ie->dtstart_array[0])? $ie->dtstart_array[0]['TZID']: null;
+      //extract event's zoneless start and end times
+      $dstart = $dz? substr($ie->dtstart_array[3], strpos($ie->dtstart_array[3],':')+1): $ie->dtstart_array[3];
+      $dend = $dz? substr($ie->dtend_array[3], strpos($ie->dtend_array[3],':')+1): $ie->dtend_array[3];
+      //convert to datetime objects using event's zone
+      $dstart = $dz? new DateTime($dstart, new DateTimeZone($dz)): new DateTime($dstart);
+      $dend = $dz? new DateTime($dend, new DateTimeZone($dz)): new DateTime($dend);
+      //set datetime objects to the web app's default zone
+      $dstart->setTimezone(new DateTimeZone(date_default_timezone_get()));
+      $dend->setTimezone(new DateTimeZone(date_default_timezone_get())); //TODO more efficient way to do this?
+      //finally re-render strings
+      $event->allday = ($dz===null); //ASSUMES no zone means all day
+      //times
+      if(!$event->allday) {
+        $event->ltstart = $dstart->format('Hi'); //for sorting
+        $event->timestart = $dstart->format($prefs->timeformat);
+      }
+      //duration
       $ddiff = $dstart->diff($dend);
       $event->duration = $ddiff->days*24*60 + $ddiff->h*60 + $ddiff->i;
+      //dates
+      $event->dstart = $dstart->format('Y-m-d');
+      $event->ldstart = $dstart->format('Ymd');
+      $event->dstartShort = $dstart->format($prefs->dateshortformat);
+      //if it's an all-day event (e.g. duration is multiple of 1440), take a day off $dend
+      if($event->allday) $dend->sub(new DateInterval("P1D"));
+      $event->dend = $dend->format('Y-m-d');
+      $event->ldend = $dend->format('Ymd');
+      $event->dendShort = $dend->format($prefs->dateshortformat);
+
+      $event->style = $cal->style; //i.e. which calendar it came from
+
+      $events->events[] = $event;
     }
-    if(!$event->allday) $event->timestart = $dstart->format($prefs->timeformat);
-    $event->style = $cal->style; //i.e. which calendar it came from
+  } //end foreach $cals as $cal
 
-    $events->events[] = $event;
+  //shuffle($events->events); //temporary for testing
+  //Sort by start date, then all day, then start time, then summary
+  usort($events->events, function($a,$b){
+    $ats = property_exists($a,'ltstart')? $a->ltstart: '0';
+    $bts = property_exists($b,'ltstart')? $b->ltstart: '0';
+    return ($a->ldstart==$b->ldstart? 
+      ($ats==$bts?
+        ($a->summary>$b->summary?1:-1)
+      : ($ats>$bts?1:-1))
+    : ($a->ldstart>$b->ldstart?1:-1));
+  });
+
+  //divvy the events up into $c
+  foreach($events->events as $event) {
+    if(array_key_exists($event->dstart,$c)) {
+      $c[$event->dstart]->events[] = $event;
+    }
   }
-} //end foreach $cals as $cal
-
-shuffle($events->events); //temporary for testing
-//Sort by start date, then all day, then start time, then summary
-usort($events->events, function($a,$b){
-  $ats = property_exists($a,'timestart')? $a->timestart: '0';
-  $bts = property_exists($b,'timestart')? $b->timestart: '0';
-  return ($a->ldstart==$b->ldstart? 
-    ($ats==$bts?
-      ($a->summary>$b->summary?1:-1)
-    : ($ats>$bts?1:-1))
-  : ($a->ldstart>$b->ldstart?1:-1));
-});
-
-//divvy the events up into $c
-foreach($events->events as $event) {
-  if(array_key_exists($event->dstart,$c)) {
-    $c[$event->dstart]->events[] = $event;
-  }
-}
+} //end if cals specified
 
 
 
 $j = array();
 foreach($c as $cd) $j[] = $cd; //convert to non-associative array
 
-//Pretend display
-?>
-<style>
-  .month, .weekday { font-size: 2em; }
-  .date { font-size: 3em; }
-</style>
-<?php
-$doneToday = false;
-foreach($c as $cd) {
-  //header
-  if(!$doneToday) {
-    echo "<p><span class='weekday'>".$cd->weekday."</span> <span class='date'>".$cd->date."</span> <span class='weekday'>".$cd->month."</span></p>";
-    //sunrise, sunset, moon
-    $doneToday = true;
-  } else {
-    //echo "<p>".$cd->weekday." ".$cd->date." ".$cd->month."</p>";
+if(isset($_REQUEST['sample'])) {
+  //Pretend display
+  foreach($c as $cd) {
+    //header
+    if($cd->weekdayRelative=='Today') {
+      echo "<h2>".$cd->weekdayShort." <span style='font-size: 1.5em;'>".$cd->date."</span> ".$cd->monthShort."</h2>";
+      //sunrise, sunset
+      if(property_exists($cd,'sun')) echo "<p>".($cd->sun['sunrise']? "Sunrise <strong>".$cd->sun['sunrise']."</strong>": "")." &nbsp; ".($cd->sun['sunset']? "Sunset <strong>".$cd->sun['sunset']."</strong>": "")."</p>";
+      //moon TODO
+    } else {
+      echo "<h3><strong>".$cd->weekdayRelative." ".$cd->dateShort."</strong></h3>";
+    }
+    //weather
+    echo "<ul style='list-style-type: none; padding-left: 0;'>";
+    foreach($cd->weather as $cw) {
+      echo "<li><strong>";
+      echo ($cw->isDaytime?'High ':'Low ');
+      //echo $cw->name.' ';
+      echo $cw->temperature."°</strong> ".$cw->shortForecast."</li>";
+    }
+    echo "</ul>";
+    //events
+    echo "<ul>";
+    foreach($cd->events as $ce) {
+      echo "<li style='color: ".$ce->style."'>";
+      if($ce->allday) echo "<strong>".$ce->summary."</strong>".($ce->dend!==$ce->dstart? " (thru ".$ce->dendShort.")": "");
+      else echo "<strong>".$ce->timestart."</strong> ".$ce->summary;
+      echo "</li>";
+    }
+    echo "</ul>";
   }
-  //weather
-  echo "<ul style='list-style-type: none; padding-left: 0;'>";
-  foreach($cd->weather as $cw) {
-    echo "<li><strong>";
-    //echo ($cw->isDaytime?'High ':'Low ');
-    echo $cw->name.' ';
-    echo $cw->temperature."°</strong> ".$cw->shortForecast."</li>";
-  }
-  echo "</ul>";
-  //events
-  echo "<ul>";
-  foreach($cd->events as $ce) {
-    echo "<li style='color: ".$ce->style."'>";
-    if($ce->allday) echo "<strong>".$ce->summary."</strong>";
-    else echo "<strong>".$ce->timestart."</strong> ".$ce->summary;
-    echo "</li>";
-  }
-  echo "</ul>";
-}
 
-echo "<pre>".json_encode($c,JSON_PRETTY_PRINT)."</pre>";
-//echo json_encode($c);
+  echo "<pre>".json_encode($j,JSON_PRETTY_PRINT)."</pre>";
+
+} else { //not sample
+  echo json_encode($j);
+}
 
 ?>
--- settings-sample.php
+++ settings-sample.php
@@ -1,34 +1,34 @@
 <?php
 date_default_timezone_set('America/Chicago');
 define('DEFAULT_TIME_FORMAT', 'H:i');
+define('DEFAULT_DATE_SHORT_FORMAT', 'm/d');
 define('DEFAULT_DAYS', 2);
-define('INCLUDE_DURATION', true);
 define('NWS_USER_AGENT', 'user agent string here');
 
-define('AUTHKEY','random string here'); //if present, this will require any requests to / to include ?auth=[authkey] in order to process
+//For generating JSON for a single calendar at a time, via base path
+define('AUTHKEY','random auth string here'); //if present, this will require any requests to / to / to include ?auth=[authkey] in order to process
 
-//For /full content generation, by auth key:
-
-//Preferences - can override the defaults above
-define('PREFS','{
-  "auth key here": {
+//For /full content JSON generation, by auth key
+//nws = National Weather Service WFO/Gridpoint
+//cals = iCalendar sources
+define('AUTHKEYS','{
+  "random auth string here": {
     "tz": "America/Chicago",
-    "timeformat": "H:i",
-    "days": 2
+    "timeformat": "G:i",
+    "dateshortformat": "n/j",
+    "days": 2,
+    "latitude": 32.55,
+    "longitude": -96.55,
+    "nws": "FWD/86,102",
+    "cals": [
+      {
+        "src": "ics url here",
+        "style": "black"
+      },
+      {
+        "src": "ics url here",
+        "style": "red"
+      }  
+    ]
   }
-}');
-
-//National Weather Service WFO/Gridpoint
-define('NWS','{
-  "auth key here": "FWD/86,102"
-}');
-
-//iCalendar URLs
-define('CALS','{
-  "auth key here": [
-    {
-      "src": "ics url here",
-      "style": "black"
-    }
-  ]
 }');
